d77778715ef4d3e3c64c9aa6223cefb895cd94ee
Elodie <elodie.ikkache@gmail.com>
2018-01-30 15:27:52 +0100

    nouvelle méthode, bien plus efficace

--- crossword.py
+++ crossword.py
@@ -2,7 +2,6 @@ from constraint_programming import constraint_programming
 import time
 from pprint import pprint
 
-ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
 
 class crossword_solveur:
 
@@ -15,9 +14,7 @@ class crossword_solveur:
         self.n = grid[3]
         self.m = grid[4]
         self.var = self.init_domain()
-        self.solver = constraint_programming(self.var)
-        #self.solver.maintain_arc_consistency()
-        self.intersection(self.H_segment, self.V_segment, self.var)
+        self.contraintes = self.intersection()
 
 
     def init_grid(self, grid_file):
@@ -34,7 +31,6 @@ class crossword_solveur:
             buff = []
             for j, elt in enumerate(line):
                 if elt == ".":
-                    grid[(i,j)] = ALPHABET
                     buff.append((i,j))
                 else:
                     if buff and len(buff) > 1:
@@ -46,7 +42,6 @@ class crossword_solveur:
         for j in range(m):
             for i, line in enumerate(row):
                 if line[j] == ".":
-                    grid[(i,j)] = ALPHABET
                     buff.append((i,j))
                 else:
                     if buff and len(buff) > 1:
@@ -88,7 +83,7 @@ class crossword_solveur:
                     dico[n] = [mot]
         return dico
 
-    def relation(self, i, j, liste_mot_h, liste_mot_v):
+    def relation(self, i, liste_mot):
         """
         A partir des points d'intersection de deux variables et de leur domaine, on détermne l'ensemble des couples
         possibles de mots pour ces deux variables. Cette relation sera ajoutée aux contraintes pour le solveur.
@@ -98,27 +93,13 @@ class crossword_solveur:
         :param liste_mot_v: liste des mots possibles pour la variable verticale
         :return: set des couples possibles de mots
         """
-        t1 = time.time()
         rel = []
-        dic_h = {}
-        for mot_h in liste_mot_h:
-            lettreh = mot_h[i]
-            try :
-                dic_h[lettreh].append(mot_h)
-            except:
-                dic_h[lettreh] = [mot_h]
-        for mot_v in liste_mot_v:
-            lettrev = mot_v[j]
-            try:
-                for m in dic_h[lettrev]:
-                    rel.append((m, mot_v))
-            except:
-                pass
-        t2 = time.time()
-        print(t2-t1)
+        for mot in liste_mot:
+            lettre = mot[i]
+            rel.append((mot, lettre))
         return set(rel)
 
-    def intersection(self, horizontal, vertical, var):
+    def intersection(self):
         """
         calcule tous les intersections entre les variables et fait appel à la fonction relation pour construire
         les relations. Elles sont ajoutée aux contraintes du solveur.
@@ -127,18 +108,26 @@ class crossword_solveur:
         :param var: dictionnaire des varibles avec leur domaine
         :return:
         """
-        for h_key, h_positions in horizontal.items():
+        contraintes = []
+        for h_key, h_positions in self.H_segment.items():
             print(h_key)
             # pour chaque section horizontale ....
-            for v_key, v_positions in vertical.items():
+            for v_key, v_positions in self.V_segment.items():
                 # ... on regarde si une section verticale l'intersecte
                 for i in range(len(h_positions)):
                     for j in range(len(v_positions)):
                         if h_positions[i] == v_positions[j]:
-                            rel = self.relation(i, j, var[h_key], var[v_key])
-                            self.solver.addConstraint(h_key, v_key, rel)
+                            print(str(h_positions[i]))
+                            self.var[str(h_positions[i])] = set(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+                                                                 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                                                                 'w', 'x', 'y', 'z'])
+                            rel_h = self.relation(i, self.var[h_key])
+                            rel_v = self.relation(j, self.var[v_key])
+                            contraintes.append((h_key, str(h_positions[i]), rel_h))
+                            contraintes.append((v_key, str(h_positions[i]), rel_v))
                             j = len(v_positions)-1
                             i = len(h_positions)-1
+        return contraintes
 
                         
     def solve(self):
@@ -146,7 +135,11 @@ class crossword_solveur:
         appel de la fonction solve du solveur pour avoir une solution possible du mot croisé
         :return: une solution possible sous form de dictionnaire avec comme valeur un mot pour chaque variable
         """
-        return self.solver.solve()
+        solver = constraint_programming(self.var)
+        for c in self.contraintes:
+            solver.addConstraint(c[0], c[1], c[2])
+        solver.maintain_arc_consistency()
+        return solver.solve()
 
     def display_solution(self):
         """
@@ -161,10 +154,12 @@ class crossword_solveur:
         for sequence in solution:
             if sequence[0] == 'H':
                 positions = self.H_segment[sequence]
-            else:
+                for i in range(len(positions)):
+                    sol[positions[i][0]][positions[i][1]] = solution[sequence][i]
+            elif sequence[0] == 'V':
                 positions = self.V_segment[sequence]
-            for i in range(len(positions)):
-                sol[positions[i][0]][positions[i][1]] = solution[sequence][i]
+                for i in range(len(positions)):
+                    sol[positions[i][0]][positions[i][1]] = solution[sequence][i]
         for ligne in sol:
             content = ""
             for col in ligne:
@@ -179,7 +174,7 @@ class crossword_solveur:
 
 if __name__ == "__main__":
     t1 = time.time()
-    c = crossword_solveur("crossword1.txt", "words1.txt")
+    c = crossword_solveur("crossword2.txt", "words2.txt")
     c.display_solution()
     t2 = time.time()
     print(t2-t1)
